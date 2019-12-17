6d2b8e877265ef1aaeec4b4d86cf2de3b2d2f7d2
tfluhr <35077186+tfluhr@users.noreply.github.com>
2019-12-17 15:29:44 -0600

    Update rename_mods_files.py

--- tech_news/rename_mods_files.py
+++ tech_news/rename_mods_files.py
@@ -1,7 +1,7 @@
 import os
 myfile = "C:\\Users\\tfluhr\\Desktop\\Oxygen Projects\\technews\\issue_xml\\mods_test\\file_V010N06.xml"
 
-home_dir = "C:\\Users\\tfluhr\\Desktop\\Oxygen Projects\\technews\\issue_xml\\mods_test\\"
+home_dir = "C:\\Users\\tfluhr\\Desktop\\Oxygen Projects\\technews\\issue_xml\\done\\"
 
 fn = 'file_V004N12.xml'
 
@@ -25,10 +25,21 @@ for root, dirs, files in os.walk(home_dir):
         else:
             isn = new_fn[9:11]
         
-        #build new file name
-        final_fn = "tnvol" + vn + "no" + isn + ".xml"
+        # Account for special issues and oddities
+        if new_fn[-5].isnumeric() == 0 and new_fn[-6].isnumeric() == 0:
+            special = new_fn[-6:-4]
+        elif new_fn[-5].isnumeric() == 0 and new_fn[-6].isnumeric() == 1:
+            special = new_fn[-5]
+        else:
+            special = "special"
+            
         
+        #build new file name
+        if special is not "special":
+            final_fn = "tnvol" + vn + "no" + isn + special + ".xml"
+        else:
+            final_fn = "tnvol" + vn + "no" + isn + ".xml"
         os.rename(home_dir + f, home_dir + final_fn)
         
-        print(final_fn)
+        #print(final_fn)
 
